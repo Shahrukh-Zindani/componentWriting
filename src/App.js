8cdb1d34d6d2883f33dd1536253f3d2a3077ec56
Shahrukh Zindani <Shahrukh.Zindani@copart.com>
2017-08-04 10:05:37 -0500

    first iteration

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import DatePickerDialog from 'material-ui/DatePicker/DatePickerDialog';
+import Dialog from 'material-ui/Dialog';
+import Example from './DatePicker';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  handleChange(selectedDate) {
+    console.log(selectedDate)
+  }
+  render() {
+
+    return (
+      <MuiThemeProvider>
+        <div className="App" >
+        <Example onChange={this.handleChange}/>
+          
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
---
+++ src/DatePicker.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css';
+import moment from 'moment';  
+import Card from 'material-ui/Card';
+import './DatePicker.css';
+
+
+class Example extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      startDate: moment(),
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(date) {
+    this.setState({
+      startDate: date,
+    })
+  }
+  
+
+  render() {
+    return (
+      
+        <div className = 'wrapper-datepicker'>
+          <DatePicker
+            
+            {...this.props}
+            selected={this.state.startDate}
+            // onChange={this.handleChange}
+            showMonthDropdown
+            showYearDropdown
+            disabled={false}
+            excludeDates={[moment(), moment().subtract(1, 'days')]}
+          />
+        </div>
+      
+    )
+  }
+}
+
+export default Example
