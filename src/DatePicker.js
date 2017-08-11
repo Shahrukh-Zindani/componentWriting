97b8b01e7ffac980e94f627f4ee4d18490b62685
Shahrukh Zindani <shahrukh.zindani@copart.com>
2017-08-11 10:30:05 -0500

    Add slashes

--- src/DatePicker.js
+++ src/DatePicker.js
@@ -5,7 +5,9 @@ import moment from 'moment';
 import Card from 'material-ui/Card';
 import './DatePicker.css';
 import TextField from 'material-ui/TextField'
-
+import { getFormatter, getCursorPositioner } from './formatter'
+const template = '**/**/****';
+export const formatPhoneNumberInput = getFormatter(template)
 
 class Example extends React.Component {
   constructor(props) {
@@ -14,13 +16,24 @@ class Example extends React.Component {
       startDate: moment(),
     }
     this.handleChange = this.handleChange.bind(this)
+    this.handleRawChange = this.handleRawChange.bind(this)
   }
 
   handleChange(date) {
-    console.log(date)
     this.setState({
       startDate: date,
     })
+  }
+
+  handleRawChange(e) {
+    if(e.which!==8) {
+      if(e.target.value.length==2 || e.target.value.length==5) {
+        e.target.value = e.target.value+'/';
+      }
+      this.setState({
+        startDate: moment(formatPhoneNumberInput(e.target.value)),
+      })
+    }
   }
   
 
@@ -29,10 +42,11 @@ class Example extends React.Component {
       
         <div className = 'wrapper-datepicker'>
           <DatePicker
-            customInput={<TextField style={{fontSize: '24px'}} onChange={this.handleChange}/>}
+            customInput={<TextField style={{fontSize: '24px'}} />}
             {...this.props}
             selected={this.state.startDate}
             onChange= {this.handleChange}
+            onChangeRaw={this.handleRawChange}
             showMonthDropdown
             showYearDropdown
             disabled={false}
